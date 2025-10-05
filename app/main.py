7d3e63dd05e39bd753d7013f187137d3931eab54
thomasteinhoff <thomas11ventura@gmail.com>
2025-10-04 22:06:27 -0300

    AAAAAAAAAAAAAAAAAAAAAAAAAAA

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import FileResponse, JSONResponse
+from fastapi.responses import FileResponse
 import os
 from .api import create_app
 
@@ -7,15 +7,12 @@ app = create_app()
 
 @app.get("/")
 async def read_root():
-    try:
-        files = os.listdir(".")
-        return JSONResponse({
-            "message": "Debug - listing files",
-            "files": files,
-            "current_dir": os.getcwd()
-        })
-    except Exception as e:
-        return JSONResponse({
-            "error": str(e),
-            "current_dir": os.getcwd()
-        })
+    if os.path.exists("index.html"):
+        return FileResponse("index.html")
+    elif os.path.exists("app/index.html"):
+        return FileResponse("app/index.html")
+    else:
+        return {"error": "index.html not found"}
+
+if os.path.exists("."):
+    app.mount("/static", StaticFiles(directory="."), name="static")
