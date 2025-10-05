21c90ce1cade1e4daa7e13ada440c26f904ec90c
thomasteinhoff <thomas11ventura@gmail.com>
2025-10-04 21:28:52 -0300

    front end serving

--- app/main.py
+++ app/main.py
@@ -1,3 +1,12 @@
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+import os
 from .api import create_app
 
 app = create_app()
+
+app.mount("/static", StaticFiles(directory=".."), name="static")
+
+@app.get("/")
+async def read_root():
+    return FileResponse("../index.html")
